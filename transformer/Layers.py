056739a310163de61bae176ef1a880bfd0e040fd
cuongnguyengit <cuongnguyen170898@gmail.com>
2020-12-28 19:51:23 +0700

    .

--- transformer/Layers.py
+++ transformer/Layers.py
@@ -1,107 +1,10 @@
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+import numpy as np
 from collections import OrderedDict
 
 from transformer.SubLayers import MultiHeadAttention, PositionwiseFeedForward
-
-from text.symbols import symbols
-
-
-class Linear(nn.Module):
-    """
-    Linear Module
-    """
-
-    def __init__(self, in_dim, out_dim, bias=True, w_init='linear'):
-        """
-        :param in_dim: dimension of input
-        :param out_dim: dimension of output
-        :param bias: boolean. if True, bias is included.
-        :param w_init: str. weight inits with xavier initialization.
-        """
-        super(Linear, self).__init__()
-        self.linear_layer = nn.Linear(in_dim, out_dim, bias=bias)
-
-        nn.init.xavier_uniform_(
-            self.linear_layer.weight,
-            gain=nn.init.calculate_gain(w_init))
-
-    def forward(self, x):
-        return self.linear_layer(x)
-
-
-class PreNet(nn.Module):
-    """
-    Pre Net before passing through the network
-    """
-
-    def __init__(self, input_size, hidden_size, output_size, p=0.5):
-        """
-        :param input_size: dimension of input
-        :param hidden_size: dimension of hidden unit
-        :param output_size: dimension of output
-        """
-        super(PreNet, self).__init__()
-        self.input_size = input_size
-        self.output_size = output_size
-        self.hidden_size = hidden_size
-        self.layer = nn.Sequential(OrderedDict([
-            ('fc1', Linear(self.input_size, self.hidden_size)),
-            ('relu1', nn.ReLU()),
-            ('dropout1', nn.Dropout(p)),
-            ('fc2', Linear(self.hidden_size, self.output_size)),
-            ('relu2', nn.ReLU()),
-            ('dropout2', nn.Dropout(p)),
-        ]))
-
-    def forward(self, input_):
-
-        out = self.layer(input_)
-
-        return out
-
-
-class Conv(nn.Module):
-    """
-    Convolution Module
-    """
-
-    def __init__(self,
-                 in_channels,
-                 out_channels,
-                 kernel_size=1,
-                 stride=1,
-                 padding=0,
-                 dilation=1,
-                 bias=True,
-                 w_init='linear'):
-        """
-        :param in_channels: dimension of input
-        :param out_channels: dimension of output
-        :param kernel_size: size of kernel
-        :param stride: size of stride
-        :param padding: size of padding
-        :param dilation: dilation rate
-        :param bias: boolean. if True, bias is included.
-        :param w_init: str. weight inits with xavier initialization.
-        """
-        super(Conv, self).__init__()
-
-        self.conv = nn.Conv1d(in_channels,
-                              out_channels,
-                              kernel_size=kernel_size,
-                              stride=stride,
-                              padding=padding,
-                              dilation=dilation,
-                              bias=bias)
-
-        nn.init.xavier_uniform_(
-            self.conv.weight, gain=nn.init.calculate_gain(w_init))
-
-    def forward(self, x):
-        x = self.conv(x)
-        return x
 
 
 class FFTBlock(torch.nn.Module):
